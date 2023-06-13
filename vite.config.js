250764a76b06ba261e493c526daa757c4cf4c2ac
BilalX7 <BilalX7>
2023-06-13 19:59:33 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'ttps://kaleem-backend.000webhostapp.com'
+      '/api': 'https://kaleem-backend.000webhostapp.com'
     }
   },
   plugins: [react()],
